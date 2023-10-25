d06d0b1175dc9be72631e154b49a2edb036db333
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2023-10-25 12:35:44 +0300

    Random y axis

--- core/src/com/example/survivorbird/SurvivorBird.java
+++ core/src/com/example/survivorbird/SurvivorBird.java
@@ -6,6 +6,8 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
+import java.util.Random;
+
 public class SurvivorBird extends ApplicationAdapter {
 
 	SpriteBatch batch;
@@ -20,8 +22,13 @@ public class SurvivorBird extends ApplicationAdapter {
 	float velocity = 0;
 	float gravity = 0.1f;
 	float enemyVelocity = 2;
+	Random random;
+
 	int numberOfEnemies = 4;
 	float [] enemyX = new float[numberOfEnemies];
+	float[] enemyOffSet = new float[numberOfEnemies];
+	float[] enemyOffSet2 = new float[numberOfEnemies];
+	float[] enemyOffSet3 = new float[numberOfEnemies];
 	float distance = 0;
 	@Override
 	public void create () {
@@ -33,11 +40,16 @@ public class SurvivorBird extends ApplicationAdapter {
 		bee3 = new Texture("bee.png");
 
 		distance = Gdx.graphics.getWidth() / 2;
+		random = new Random();
 
 		birdX = Gdx.graphics.getWidth()/2 - bird.getHeight() /2;
 		birdY = Gdx.graphics.getHeight()/3;
 
 		for (int i = 0; i< numberOfEnemies; i++){
+			enemyOffSet[i] = (random.nextFloat() - 0.5f) * (Gdx.graphics.getHeight()-200);
+			enemyOffSet2[i] = (random.nextFloat() - 0.5f) * (Gdx.graphics.getHeight()-200);
+			enemyOffSet3[i] = (random.nextFloat() - 0.5f) * (Gdx.graphics.getHeight()-200);
+
 			enemyX[i] = Gdx.graphics.getWidth() - bee1.getWidth() /2 + i*distance;
 		}
 	}
@@ -56,13 +68,17 @@ public class SurvivorBird extends ApplicationAdapter {
 			for (int i = 0; i<numberOfEnemies; i++){
 				if (enemyX[i]< Gdx.graphics.getWidth() / 15){
 					enemyX[i] = enemyX[i] + numberOfEnemies * distance;
+
+					enemyOffSet[i] = (random.nextFloat() - 0.5f) * (Gdx.graphics.getHeight()-200);
+					enemyOffSet2[i] = (random.nextFloat() - 0.5f) * (Gdx.graphics.getHeight()-200);
+					enemyOffSet3[i] = (random.nextFloat() - 0.5f) * (Gdx.graphics.getHeight()-200);
 				}else {
 					enemyX[i] = enemyX[i] - enemyVelocity;
 				}
 				enemyX[i] = enemyX[i] - enemyVelocity;
-				batch.draw(bee1,enemyX[i],50, Gdx.graphics.getWidth() / 15, Gdx.graphics.getHeight() / 10);
-				batch.draw(bee2,enemyX[i],150, Gdx.graphics.getWidth() / 15, Gdx.graphics.getHeight() / 10);
-				batch.draw(bee3,enemyX[i],350, Gdx.graphics.getWidth() / 15, Gdx.graphics.getHeight() / 10);
+				batch.draw(bee1,enemyX[i],Gdx.graphics.getHeight()/2 + enemyOffSet[i], Gdx.graphics.getWidth() / 15, Gdx.graphics.getHeight() / 10);
+				batch.draw(bee2,enemyX[i],Gdx.graphics.getHeight()/2 + enemyOffSet2[i], Gdx.graphics.getWidth() / 15, Gdx.graphics.getHeight() / 10);
+				batch.draw(bee3,enemyX[i],Gdx.graphics.getHeight()/2 + enemyOffSet3[i], Gdx.graphics.getWidth() / 15, Gdx.graphics.getHeight() / 10);
 			}
 
 
